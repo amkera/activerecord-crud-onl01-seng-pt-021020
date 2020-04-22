87d5876c44fbf3b3c811ad380c596fa50023bfab
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-22 04:27:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -15,7 +15,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.find_or_create_by(attributes)
 end
 
-def can_be_created_in_a_block()
+def can_be_created_in_a_block(movie = {title: "Home Alone", release_date: 1990})
   Movie.create do |m|
     m.title="Home Alone"
     m.release_date=1990
