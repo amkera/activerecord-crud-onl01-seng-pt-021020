6212c0b8ee94dc34718b518e0348a87fc392b697
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-22 04:13:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,8 +25,8 @@ def can_be_created_in_a_block(movie = {title: "Home Alone", release_date: 1990})
   # release_date == 1990
   
   Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
+    m.title = movie[:title]
+    m.release_date = movie[:release_date]
   end
 end
 
