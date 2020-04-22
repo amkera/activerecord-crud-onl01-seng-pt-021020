ea09c6ba34710d2632f157655066a89b088cbc2e
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-22 04:25:02 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -15,7 +15,7 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = Movie.create(attributes)
+  movie = Movie.find_or_create_by(attributes)
 end
 
 def can_be_created_in_a_block(movie = {title: "Home Alone", release_date: 1990})
